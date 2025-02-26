4a3b94cddcfe1ce5c34000024052653764f49be8
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-02-25 23:36:25 -0300

    Edit_calendar is moved from birthday_reminder.ipynb to file_manager.py

--- tools/file_manager.py
+++ tools/file_manager.py
@@ -14,7 +14,22 @@ def initialize_calendar(path_calendar: Path) -> pd.DataFrame:
         return df
     else:
         return pd.read_csv(path_calendar, index_col=0)
-    
+
+def edit_calendar(df: pd.DataFrame, path_calendar:Path) -> pd.DataFrame:
+#requesting data from the user
+    id_row = int(input("\Set the row ID to modify"))
+    column = input("Enter the column name to modify")
+    new_value = input("Enter the new value")
+    #modifying the value
+    if column in df.columns and id_row in df["ID"].values:
+        df.loc[df["ID"] == id_row, column] = new_value
+        # Save modified file
+        df.to_csv(path_calendar, index= False)
+        print("Modification successful")
+    else:
+        ("ID or column not founded")
+    return df
+
 def save_calendar(df:pd.DataFrame, path_calendar:Path) -> None:
     """ Save the dataframe to the calendar file"""
     df.to_csv(path_calendar)
