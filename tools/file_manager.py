2894759967f6a04afbfe20d370038192b34f7b29
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-02-25 21:57:40 -0300

    Function initialize_calendar is moved from birthday_reminder.ipynb to file_manager.py

--- tools/file_manager.py
+++ tools/file_manager.py
@@ -5,3 +5,12 @@ def create_empty_df() -> pd.DataFrame:
     """Create an empty dataframe with the proper columns"""
     columns = ["name", "birthday", "gift"]
     return pd.Dataframe(columns=columns)
+
+def initialize_calendar(path_calendar: Path) -> pd.DataFrame:
+    """Initialize or load the calendar file"""
+    if not path_calendar.exists():
+        df = create_empty_df()
+        df.to_csv(path_calendar)
+        return df
+    else:
+        return pd.read_csv(path_calendar, index_col=0)
