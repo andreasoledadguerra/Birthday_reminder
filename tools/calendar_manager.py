b374c3a45063981051dce2f4f35a24a3a12ca961
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-02-25 22:13:14 -0300

    Function add_row is moved from birthday_reminder.ipynb to calendar_manager.py

--- tools/calendar_manager.py
+++ tools/calendar_manager.py
@@ -1,3 +1,8 @@
 import pandas as pd
 from pathlib import Path
 
+def add_row(df:pd.DataFrame, row: list) -> pd.DataFrame:
+    """Add a new row to the calendar" dataframe"""
+
+    df_new_row = pd.DataFrame([row], columns=df.columns)
+    return pd.concat([df, df_new_row], ignore_index=True)
