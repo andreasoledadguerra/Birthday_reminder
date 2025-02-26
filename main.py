edc66c396ca7d4c9f936720d30e2bc34f65b8f79
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-02-25 23:41:02 -0300

    Function edit_calendar is added to folcer main.py. Imported from file_manager.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from pathlib import Path
-from tools.file_manager import initialize_calendar, save_calendar
+from tools.file_manager import initialize_calendar, edit_calendar, save_calendar
 
 def main():
     """Main entry point for the birthday reminder application"""
     path_calendar = Path("calendar.csv")
 
     df = initialize_calendar(path_calendar)
+
+    edit_calendar(df, path_calendar)
 
     save_calendar(df, path_calendar)
 
