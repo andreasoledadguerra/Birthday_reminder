9e8858206c1d7c88811737ebdb013482238ca36d
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-03-04 09:59:33 -0300

    'file_manager' is upgraded adding a new option in the main menu

--- tools/file_manager.py
+++ tools/file_manager.py
@@ -18,25 +18,35 @@ def initialize_calendar(path_calendar: Path) -> pd.DataFrame:
 def edit_calendar(df: pd.DataFrame, path_calendar:Path) -> pd.DataFrame:
     """Edit or add new data on the calendar file"""
     is_choice_ok = False
+    row = [id, name, birthday, gift]
+    id = ""
     name = ""
     birthday = ""
     gift = ""
     while not is_choice_ok:
         #requesting data from the user
-        choice = input("Choose an option: 1. Add name \n 2. Add birthday \n 3. Add gift \n 4. Repet options")
+        choice = input("Choose an option: 1. Choose row to edit \n 2. Add name \n 3. Add birthday \n 4. Add gift \n 5. Repeat options")
         choice = int(choice)
+
         if choice == 1:
-            name = input(f"Please, enter de new name: ")
+            #Si el usuario elije editar una fila pre-existente, mejor mostrarle el dataframe
+            print(df.to_string())
+            id = input("Please, enter the ID you want to modify")
+            is_choice_ok = True
+
+        elif choice == 2:
+            name = input("Please, enter de new name: ")
             is_choice_ok = True
                   
-        elif choice == 2:
+        elif choice == 3:
             birthday = input("Please, enter the new birthday (YYY-MM-DD): ")
             is_choice_ok = True
-        elif choice == 3:
+
+        elif choice == 4:
             gift = input("Please, enter the new gift: ")
             is_choice_ok = True
         else: 
-             is_choice_ok == 4
+             is_choice_ok == 5
              continue
     return pd.read_csv(path_calendar, index_col=0)
    
