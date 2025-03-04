b1fc918355d9c913f4e1227dd8b0d7f19d2931fd
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-03-04 18:49:17 -0300

    'edit_calendar' is upgraded

--- tools/file_manager.py
+++ tools/file_manager.py
@@ -26,7 +26,7 @@ def edit_calendar(df: pd.DataFrame, path_calendar:Path) -> pd.DataFrame:
     while not is_choice_ok:
         #requesting data from the user
         
-        id = input(f"Please, enter the ID you want to modify \n{df.to_string()}")
+        id = input(f"Please, enter the ID or index you want to modify \n{df.to_string()}")
         
         choice = input("Choose the data you want to modify:\n 1. Name\n 2. Birthday\n 3. Gift\n 4. Repeat options")
         try:
@@ -38,17 +38,17 @@ def edit_calendar(df: pd.DataFrame, path_calendar:Path) -> pd.DataFrame:
 
         if choice == 1:
             name = input("Please, enter de new name: ")
-            name = df.loc[df["ID"] == id, "name"]
+            name = df.loc[id, name]
             is_choice_ok = True
                   
         elif choice == 2:
             birthday = input("Please, enter the new birthday (YYY-MM-DD): ")
-            birthday = df.loc[df["ID"] == id, "birthday"]
+            birthday = df.loc[id, birthday]
             is_choice_ok = True
 
         elif choice == 3:
             gift = input("Please, enter the new gift: ")
-            gift = df.loc[df["ID"]== id, "gift"]
+            gift = df.loc[id, gift]
             is_choice_ok = True
         else: 
              choice == 4
